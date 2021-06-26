0a47005558ae2b22be73b4ea39684584542fee5d
che <che27012011@googlemail.com>
2021-06-26 18:22:33 +0200

    adapt to changes of config file

--- streamlink-script.py
+++ streamlink-script.py
@@ -1,3 +1,4 @@
+#!python
 import shutil
 import sys
 import os.path
@@ -10,8 +11,8 @@ os.environ['PYTHONPATH'] = os.pathsep.join([pkgdir, os.environ.get('PYTHONPATH',
 if __name__ == '__main__':
     from streamlink_cli.main import main
 
-    # install the streamlinkrc file, if one is not installed
-    if not os.path.exists(os.path.join(basedir, "streamlinkrc")):
-        shutil.copyfile(os.path.join(basedir, "streamlinkrc.default"),
-                        os.path.join(basedir, "streamlinkrc"))
+    # install the config file, if one is not installed
+    if not os.path.exists(os.path.join(basedir, "config")):
+        shutil.copyfile(os.path.join(basedir, "config.default"),
+                        os.path.join(basedir, "config"))
     main()

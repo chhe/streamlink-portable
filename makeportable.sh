082e2189922ccec8b13708ee34e9491cb2e1d52f
che <che27012011@googlemail.com>
2022-04-16 00:13:08 +0200

    add versioningit dependency

--- makeportable.sh
+++ makeportable.sh
@@ -86,7 +86,7 @@ cd "${STREAMLINK_REPO_DIR}"
 git checkout .
 
 ${PIP_EXECUTABLE} download --only-binary ":all:" --platform "${PYTHON_PLATFORM}" --python-version "${STREAMLINK_PYTHON_VERSION}" --implementation "cp" -d "${temp_dir}" "pycryptodome>=3.4.3,<4.0" "lxml>=4.6.4,<5.0"
-${PIP_EXECUTABLE} install -t "${packages_dir}" "pycountry" "setuptools" "requests>=2.26.0,<3.0" "websocket-client>=0.58.0" "PySocks!=1.5.7,>=1.5.6" "isodate"
+${PIP_EXECUTABLE} install -t "${packages_dir}" "pycountry" "setuptools" "requests>=2.26.0,<3.0" "websocket-client>=0.58.0" "PySocks!=1.5.7,>=1.5.6" "isodate" "versioningit"
 
 cd "${STREAMLINK_REPO_DIR}"
 
